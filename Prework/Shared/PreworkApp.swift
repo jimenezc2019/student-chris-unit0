f1dbd56de9e276b528cc002ca846f851a217de10
jimenezc2019 <jimenezc2019@fau.edu>
2022-08-27 22:35:48 -0400

    Added prework files for IOS tip calculator to repository

---
+++ Prework/Shared/PreworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreworkApp.swift
+//  Shared
+//
+//  Created by Chris Jimenez on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct PreworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
